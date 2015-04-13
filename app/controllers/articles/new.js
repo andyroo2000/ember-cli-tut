0c8bb4b0a901bb17e3ea6bd5a28e66b66e2a7f92
Andrew Landry <andrewlandry@gmail.com>
2015-04-13 00:10:35 -0400

    add validation to new articles form in controller

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    function() {
+      return !Ember.isEmpty(this.get('model.description'));
+    }
+  ),
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        this.get('model').save().then(function() {
+
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill in all of the fields');
+      }
+      return true;
+    }
+  }
+});
