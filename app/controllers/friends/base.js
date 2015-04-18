99b1181823f01465966abd4cd737d3fa972f1eb4
Andrew Landry <andrewlandry@gmail.com>
2015-04-18 18:02:52 -0400

    correct typo in friends/new validation

--- app/controllers/friends/base.js
+++ app/controllers/friends/base.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   hasEmail:     Ember.computed.notEmpty('model.email'),
-  hasFirstName: Ember.computed.notEmpty('model.firsName'),
+  hasFirstName: Ember.computed.notEmpty('model.firstName'),
   hasLastName:  Ember.computed.notEmpty('model.lastName'),
   hasTwitter:   Ember.computed.notEmpty('model.twitter'),
   isValid:      Ember.computed.and(
