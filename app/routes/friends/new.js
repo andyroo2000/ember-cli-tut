543a19d2b9e7a0969f038f377447e567e77c3267
Andrew Landry <andrewlandry@gmail.com>
2015-04-18 17:08:35 -0400

    destroy new friend record if deactivate hook is called in the route

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,13 +4,18 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
+  deactivate: function() {
+    var model = this.modelFor('friends/new');
+
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
   actions: {
     save: function() {
-      // console.log('+-- save action bubbled up to freinds new route')
       return true;
     },
     cancel: function() {
-      // console.log('+-- cancel action bubbled up to freinds new route')
       return true;     
     }
   }
