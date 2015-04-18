6af423a77fb73335ca6d5b99875c507a7d846c16
Andrew Landry <andrewlandry@gmail.com>
2015-04-18 17:16:33 -0400

    destroy articles when deactivation hook is called

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -4,6 +4,13 @@ export default Ember.Route.extend({
   model: function() {
     return this.modelFor('friends/show').get('articles');
   },
+  deactivate: function() {
+    var model = this.modelFor('articles/new');
+
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
   actions: {
     save: function(model) {
       model.save();
